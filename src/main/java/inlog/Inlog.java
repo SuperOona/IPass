cdb64186a04895aafe88ee28d9cec3cb047a3e9d
Oona <oona.bertrums@student.hu.nl>
2021-05-20 18:05:19 +0200

    Sprint 1

---
+++ src/main/java/inlog/Inlog.java
@@ -0,0 +1,15 @@
+package inlog;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Inlog {
+    private LocalDate today;
+    private ArrayList<LocalDate> dates;
+
+    public Inlog(LocalDate today, ArrayList<>)
+
+    public void toevoegen(){
+        dates.add(today);
+    }
+}
